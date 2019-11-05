6e796c123b17bbc7bca50d336d4eeed17f5e4e27
Kat M <mcLcomdata@protonmail.com>
2019-11-04 16:47:00 -0800

    chapter 1 part 3

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-let toggleHidden = function() {
+let toggleHidden1 = function() {
   document.querySelector("#felix").classList.toggle("hidden")
   document.querySelector("#joen").classList.toggle("hidden")
 }
 
 let felixButton = document.querySelector("#felix-button");
 
-felixButton.addEventListener("click", toggleHidden)
+felixButton.addEventListener("click", toggleHidden1)
+
+let toggleHidden2 = function() {
+  document.querySelector("#joen").classList.toggle("hidden")
+  document.querySelector("#email").classList.toggle("hidden")
+}
+
+let joenButton = document.querySelector("#joen-button")
+
+joenButton.addEventListener("click", toggleHidden2)

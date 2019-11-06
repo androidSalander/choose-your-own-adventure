fed9c329b8fa0f47857cc9379e23e1e56b6e0a4c
Kat M <mcLcomdata@protonmail.com>
2019-11-06 13:41:21 -0800

    added choices and back buttons

--- app.js
+++ app.js
@@ -1,103 +1,150 @@
+let felix = document.querySelector("#felix")
+let joen = document.querySelector("#joen")
 
 let toggleHidden1 = function() {
-  document.querySelector("#felix").classList.toggle("hidden")
-  document.querySelector("#joen").classList.toggle("hidden")
+  felix.classList.toggle("hidden")
+  joen.classList.toggle("hidden")
 }
 
 let felixButton = document.querySelector("#felix-button");
 
 felixButton.addEventListener("click", toggleHidden1)
 
+let email = document.querySelector("#email")
+
+let goBack = function() {
+  joen.classList.toggle("hidden")
+  felix.classList.toggle("hidden")
+}
+
+let joenBackButton = document.querySelector("#joen-back-button")
+
+joenBackButton.addEventListener("click", goBack)
+
 let toggleHidden2 = function() {
-  document.querySelector("#joen").classList.toggle("hidden")
-  document.querySelector("#email").classList.toggle("hidden")
+  joen.classList.toggle("hidden")
+  email.classList.toggle("hidden")
 }
 
 let joenButton = document.querySelector("#joen-button")
 
 joenButton.addEventListener("click", toggleHidden2)
 
-let toggleHidden3 = function() {
-  document.querySelector("#email").classList.toggle("hidden")
-  document.querySelector("#field-work").classList.toggle("hidden")
+let goBack2 = function() {
+  email.classList.toggle("hidden")
+  joen.classList.toggle("hidden")
+}
+
+let emailBackButton = document.querySelector("#email-back-button")
+
+emailBackButton.addEventListener("click", goBack2)
+
+let choiceCuffInstall = document.querySelector("#choice-cuff-install")
+
+let choicePrompt = function() {
+  email.classList.toggle("hidden")
+  choiceCuffInstall.classList.toggle("hidden")
 }
 
 let emailButton = document.querySelector("#email-button")
 
-emailButton.addEventListener("click", toggleHidden3)
+emailButton.addEventListener("click", choicePrompt)
+
+let goBack3 = function() {
+  choiceCuffInstall.classList.toggle("hidden")
+  email.classList.toggle("hidden")
+}
+
+let choiceCuffInstallBackButton = document.querySelector("#choice-cuff-install-back-button")
+
+choiceCuffInstallBackButton.addEventListener("click", goBack3)
+
+let cableFail = document.querySelector("#cable-fail")
+
+let playerChoose1 = function() {
+  choiceCuffInstall.classList.toggle("hidden")
+  cableFail.classList.toggle("hidden")
+}
+
+let cableRep = document.querySelector("#cable-rep")
+
+cableRep.addEventListener("click", playerChoose1)
+
+let goBack4 = function() {
+  cableFail.classList.toggle("hidden")
+  choiceCuffInstall.classList.toggle("hidden")
+}
+
+let cableFailBackButton = document.querySelector("#cable-fail-back-button")
+
+cableFailBackButton.addEventListener("click", goBack4)
+
+let fieldWork = document.querySelector("#field-work")
+
+let playerChoose2 = function() {
+  choiceCuffInstall.classList.toggle("hidden")
+  fieldWork.classList.toggle("hidden")
+}
+
+let fakeTeen = document.querySelector("#fake-teen")
+
+fakeTeen.addEventListener("click", playerChoose2)
+
+let goBack5 = function() {
+  fieldWork.classList.toggle("hidden")
+  choiceCuffInstall.classList.toggle("hidden")
+}
+
+let fieldWorkBackButton = document.querySelector("#field-work-back-button")
+
+fieldWorkBackButton.addEventListener("click", goBack5)
+
+let chapter1 = document.querySelector("#chapter1")
+let chapter2 = document.querySelector("#chapter2")
+let blomkvist = document.querySelector("#blomkvist")
 
 let toggleHidden4 = function() {
-  document.querySelector("#chapter1").classList.toggle("hidden")
-  document.querySelector("#field-work").classList.toggle("hidden")
-  document.querySelector("#chapter2").classList.toggle("hidden")
-  document.querySelector("#blomkvist").classList.toggle("hidden")
+  chapter1.classList.toggle("hidden")
+  fieldWork.classList.toggle("hidden")
+  chapter2.classList.toggle("hidden")
+  blomkvist.classList.toggle("hidden")
 }
 
 let fieldWorkButton = document.querySelector("#field-work-button")
 
 fieldWorkButton.addEventListener("click", toggleHidden4)
 
-let toggleHidden5 = function() {
-  document.querySelector("#blomkvist").classList.toggle("hidden")
-  document.querySelector("#lunch").classList.toggle("hidden")
+let goBack6 = function() {
+  blomkvist.classList.toggle("hidden")
+  chapter2.classList.toggle("hidden")
+  fieldWork.classList.toggle("hidden")
+  chapter1.classList.toggle("hidden")
+}
+
+let blomkvistBackButton = document.querySelector("#blomkvist-back-button")
+
+blomkvistBackButton.addEventListener("click", goBack6)
+
+let lunchWhere = document.querySelector("#lunch-where")
+
+let choicePrompt2 = function() {
+  blomkvist.classList.toggle("hidden")
+  lunchWhere.classList.toggle("hidden")
 }
 
 let blomkvistButton = document.querySelector("#blomkvist-button")
 
-blomkvistButton.addEventListener("click", toggleHidden5)
+blomkvistButton.addEventListener("click", choicePrompt2)
 
-let toggleHidden6 = function() {
-  document.querySelector("#lunch").classList.toggle("hidden")
-  document.querySelector("#stakeout").classList.toggle("hidden")
+let goBack7 = function() {
+  lunchWhere.classList.toggle("hidden")
+  blomkvist.classList.toggle("hidden")
 }
 
-let lunchButton = document.querySelector("#lunch-button")
+let lunchWhereBackButton = document.querySelector("#lunch-where-back-button")
 
-lunchButton.addEventListener("click", toggleHidden6)
+lunchWhereBackButton.addEventListener("click", goBack7)
 
-let toggleHidden7 = function() {
-  document.querySelector("#stakeout").classList.toggle("hidden")
-  document.querySelector("#insomnia").classList.toggle("hidden")
+let playerChoose3 = function() {
+  
 }
-
-let stakeoutButton = document.querySelector("#stakeout-button")
-
-stakeoutButton.addEventListener("click", toggleHidden7)
-
-let toggleHidden8 = function() {
-  document.querySelector("#insomnia").classList.toggle("hidden")
-  document.querySelector("#killing-time").classList.toggle("hidden")
-}
-
-let insomniaButton = document.querySelector("#insomnia-button")
-
-insomniaButton.addEventListener("click", toggleHidden8)
-
-let toggleHidden9 = function() {
-  document.querySelector("#killing-time").classList.toggle("hidden")
-  document.querySelector("#idun").classList.toggle("hidden")
-}
-
-let killingTimeButton = document.querySelector("#killing-time-button")
-
-killingTimeButton.addEventListener("click", toggleHidden9)
-
-let toggleHidden10 = function() {
-  document.querySelector("#chapter2").classList.toggle("hidden")
-  document.querySelector("#idun").classList.toggle("hidden")
-  document.querySelector("#chapter3").classList.toggle("hidden")
-  document.querySelector("#meeting").classList.toggle("hidden")
-}
-
-let idunButton = document.querySelector("#idun-button")
-
-idunButton.addEventListener("click", toggleHidden10)
-
-let toggleHidden11 = function() {
-  document.querySelector("#meeting").classList.toggle("hidden")
-  document.querySelector("#camera").classList.toggle("hidden")
-}
-
-let meetingButton = document.querySelector("#meeting-button")
-
-meetingButton.addEventListener("click", toggleHidden11)
